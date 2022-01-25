141594aa624c4581a98caaf62498a255621ed005
James Broucek <jamesbroucek@Jamess-iMac-2.local>
2022-01-24 16:02:21 -0800

    404 page

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+require('dotenv').config();
+
 //requiring express
-const express = require('express')
+const express = require('express');
 //initializing express app variable
-const app = express()
+const app = express();
 
 
 //making a homepage route
 app.get('/', (req, res) => {
     res.send('We Did It')
-})
+});
+
+//making a wildcard route
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+});
 
 //Opening the server to listen for connections
-app.listen(3000)
+app.listen(process.env.PORT);
